e41cb943376eeb0020db387ca09615d0bd7a754c
Anthony Libut <antho@Anthonys-MacBook-Pro.local>
2025-01-29 02:58:30 +0800

    Added initial header??

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,26 @@
+import Button from "@ui/Button";
+import Input from "@ui/Input";
+import { Menu, Search, User } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <Button className='menu-button'>
+        <Menu className='icon' />
+      </Button>
+
+        <div className='search-bar'>
+          <Search className='search-icon'/>
+          <Input
+            type='text'
+            placeholder='Search...'
+            className='search-input'
+          />
+        </div>
+
+      <Button className='profile-button'>
+        <User className='icon'/>
+      </Button>
+    </header>
+  )
+}
---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,9 @@
+export default function Input({ type, placeholder, className }) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={className}
+    />
+  );
+}
