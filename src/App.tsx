35194208a3a297fa85347d93b99d8dba184887f7
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-02-03 04:01:26 +0800

    forgor to push this

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
 import Header from "@/components/header";
 import Main from "@/pages/main";
 import Profile from "@/pages/profile";
+import PostWindow from "./pages/postwindow";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
@@ -19,6 +20,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/post" element={<PostWindow />} />
       </Routes>
     </Router>
   </React.StrictMode>
