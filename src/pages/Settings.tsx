0601f2db085d8ede8ad7b76381b13a13c1feb2d4
LanLan12 <143952542+NebulaRojo9@users.noreply.github.com>
2025-02-13 00:13:58 +0800

    Added UserPosts, UserComments, and Settings whilst modifying global css

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,25 @@
 export default function Settings() {
   return (
     <div>
-
+      <h2>Edit Profile</h2>
+      <ul>
+        <li>Change character avatar</li>
+        <li>Change username</li>
+        <li>Change bio</li>
+        <li>Change email</li>
+      </ul>
+      <h2>Web Interface</h2>
+      <ul>
+        <li>Select theme light or dark</li>
+        <li>Customize Font style</li>
+        <li>Customize font-size</li>
+      </ul>
+      <h2></h2>
+      <h2>User Privacy</h2>
+      <ul>
+        <li>Allow username publicly displayed? </li>
+      </ul>
+      
     </div>
   )
 }
--- src/pages/UserComments.tsx
+++ src/pages/UserComments.tsx
@@ -1,7 +1,31 @@
 export default function UserComments() {
   return (
     <div>
+      <h1>History</h1>
+      <div className="user-comment-container">
+        <div className="user-comment-header">
+          Glasses | by Fuboki | a day ago
+        </div>
+        <div className="user-comment-section">
+          <div className="user-comment-entry">
+            12/02/2025 11:43pm -&gt;
+            I'm glad you like them too!    
+          </div>
+          <div className="user-comment-entry">
+            date time -&gt; sample comment  
+          </div>
+          <div className="user-comment-entry">
+            date time -&gt; Yummerzz haha   
+          </div>
+          <div className="user-comment-entry">
+            date time -&gt; Valentines  
+          </div>
+          <div className="user-comment-entry">
+            date time -&gt; sample comment  
+          </div>
+        </div>
 
+      </div>
     </div>
   )
 }
