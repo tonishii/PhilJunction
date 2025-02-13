be622594012bcdef60efd894cad6a989819fa8de
jemire22 <jericho_reyes@dlsu.edu.ph>
2025-02-13 22:50:35 +0800

    Made style changes to comments

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,4 +1,5 @@
 import { PostComment } from "@/mockdata/post-data"
+import { PencilRuler, Reply } from "lucide-react"
 
 export default function Comment({
   comment,
@@ -7,25 +8,26 @@ export default function Comment({
 }) {
 
   function handleDate(date: Date) {
-    return "A day ago.";
+    return "A day ago";
   }
 
   function handleComments(comment: PostComment): JSX.Element {
     return (
-      <div className="comment">
+      <div className="comment comment-padding">
         <div className="comment-header">
           <span className="comment-name">
             {comment.username}
           </span> 
-          
-          <span> replying to </span>
-          <span className="comment-name">
-            {comment.replyTo}
-          </span>
-
           <span className="comment-date">
-            {handleDate(comment.postDate)}
+            &nbsp;&sdot; {handleDate(comment.postDate)}
           </span>
+          <span className="comment-icons">  <Reply /><PencilRuler /></span>
+          <div className="comment-reply"> 
+            reply to 
+            <span className="comment-reply-name">
+              &nbsp;{comment.replyTo}
+            </span>
+          </div>
         </div>
     
         <p>{comment.content}</p>
