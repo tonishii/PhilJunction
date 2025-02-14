0c0fb8d1217a876d367539c2f1a039d14cb0dd2b
RD-Tan <ross_tan@dlsu.edu.ph>
2025-02-15 03:45:15 +0800

    removed header when in login/signup page

--- src/pages/authlayout.tsx
+++ src/pages/authlayout.tsx
@@ -5,28 +5,31 @@ import { Link, Outlet } from "react-router"
 
 export default function AuthLayout() {
   return (
-    <div className="login-background">
-      <div className="login-center">
-        <div className="login-header">
-          <div className="login-header-text">
-            <Logo className="login-logo" /> PhilJunction!
+    <>
+      <style rel='stylesheet'>{".header {display:none}"}</style>
+      <div className="login-background">
+        <div className="login-center">
+          <div className="login-header">
+            <div className="login-header-text">
+              <Logo className="login-logo" /> PhilJunction!
+            </div>
+            <div className="login-header-caption">
+              Your one-stop transit forum
+            </div>
+            <div className="login-signup">
+              {" "}
+              <Link to="/auth/login" className="login-signup">
+                Log In
+              </Link> |{" "}
+              <Link to="/auth/signup" className="login-signup">
+                Sign Up
+              </Link>{" "}
+            </div>
           </div>
-          <div className="login-header-caption">
-            Your one-stop transit forum
-          </div>
-          <div className="login-signup">
-            {" "}
-            <Link to="/auth/login" className="login-signup">
-              Log In
-            </Link> |{" "}
-            <Link to="/auth/signup" className="login-signup">
-              Sign Up
-            </Link>{" "}
-          </div>
-        </div>
 
-        <Outlet />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
