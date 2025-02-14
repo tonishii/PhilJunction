47a8e7475489096e2323e2892b5b2656677d7a9e
tonishii <anthonylibut@gmail.com>
2025-02-15 02:34:25 +0800

    Updated search page layout

--- src/pages/searchpage.tsx
+++ src/pages/searchpage.tsx
@@ -4,40 +4,34 @@ import postData from "@/mockdata/post-data";
 
 export default function SearchPage() {
     return (
-    <div className="outer-search-container">
         <div className="search-container">
-            <div className="outer-search-body">
-                <div className="search-body">
-                    <label htmlFor="keywords">Keywords: </label>
-                    <input type="text" id="keywords" name="keywords" className="field-clicked" />
-                    <label htmlFor="tags">Tags: </label>
-                    <input type="text" id="tags" name="tags" className="field-clicked" />
-                    <label htmlFor="sort-by">Sort by: </label>
-                    <select name="sort-by" id="sort-by">
-                        <option value="recent">Most Recent</option>
-                        <option value="liked">Most Liked</option>
-                    </select>
-                    <label htmlFor="filter-by">Filter by Time: </label>
-                    <select name="filter-by" id="filter-by">
-                        <option value="today">Today</option>
-                        <option value="week">Last Week</option>
-                        <option value="month">Last Month</option>
-                        <option value="NA">None</option>
-                    </select>
-                    <button className="search-buttons">Clear</button>
-                    <button className="search-buttons">Search</button>
-                </div>
+            <div className="search-body">
+                <label htmlFor="keywords">Keywords: </label>
+                <input type="text" id="keywords" name="keywords" className="field-clicked" />
+                <label htmlFor="tags">Tags: </label>
+                <input type="text" id="tags" name="tags" className="field-clicked" />
+                <label htmlFor="sort-by">Sort by: </label>
+                <select name="sort-by" id="sort-by">
+                    <option value="recent">Most Recent</option>
+                    <option value="liked">Most Liked</option>
+                </select>
+                <label htmlFor="filter-by">Filter by Time: </label>
+                <select name="filter-by" id="filter-by">
+                    <option value="today">Today</option>
+                    <option value="week">Last Week</option>
+                    <option value="month">Last Month</option>
+                    <option value="NA">None</option>
+                </select>
+                <button className="search-buttons">Clear</button>
+                <button className="search-buttons">Search</button>
             </div>
-            <div className="search-list">
-                <div className="search-results">Results: </div>
-                <div className="search-posts">
-                    <br />
-                    <Post post={postData}/>
-                    <br />
-                    <Post post={postData}/>
+
+            <hr />
+            <h1 className="result-text">Results: </h1>
+            <div className="search-posts">
+                <Post post={postData}/>
+                <Post post={postData}/>
             </div>
         </div>
-        </div>
-    </div>
     );
 }
