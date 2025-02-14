b7c36cb9ac4ea0b1e008646bf9bfe6246fea2556
tonishii <anthonylibut@gmail.com>
2025-02-15 03:26:34 +0800

    Added more post data

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,12 @@
 import Post from "@/components/post.tsx";
 import SmallPost from "@/components/smallpost";
-import { post1 } from "@/mockdata/more-post-data";
+import { post1, post2, post3, post4, post5, post6 } from "@/mockdata/more-post-data";
 import postData from "@/mockdata/post-data.ts";
 
 import { Flame } from "lucide-react";
 
 export default function Main() {
-  const data = [postData, post1]
+  const data = [postData, post1, post2, post3, post4, post5, post6]
   return (
     <div className="main-container">
       <div className="left-container">
@@ -21,7 +21,8 @@ export default function Main() {
       <div className="popular-posts-list">
         <span className="popular-posts-header">Trending Posts <Flame className="icon" /></span>
         {[...Array(5)].map(() =>
-          <SmallPost post={postData} />
+          <SmallPost post={data[
+            Math.floor(Math.random() * data.length)]} />
         )}
       </div>
     </div>
