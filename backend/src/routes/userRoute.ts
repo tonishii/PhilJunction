2e8c57f96cd06238ae0fd703b35177bf2b04f354
Anthony Libut <anthonylibut@Anthonys-MacBook-Pro.local>
2025-03-14 14:54:53 +0800

    Modified project structure, changed routing to profile page.

---
+++ backend/src/routes/userRoute.ts
@@ -0,0 +1,8 @@
+import express, { Router, Request, Response } from "express";
+const router: Router = express.Router();
+
+router.get('/user', (req: Request, res: Response) => {
+
+});
+
+export default router;
