e7ded3bf7b667da84569674cacb087b96761b32a
RD-Tan <ross_tan@dlsu.edu.ph>
2025-03-18 17:00:45 +0800

    removed index.ts + added default user creation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,6 +9,8 @@ import postRoute from "./routes/postRoute";
 import userRoute from "./routes/userRoute";
 import commentRoute from "./routes/commentRoute";
 import voteRoute from "./routes/voteRoute";
+import User, { IUser } from "./models/user";
+import { FilterQuery, Model, RootFilterQuery, Schema } from "mongoose";
 
 dotenv.config();
 
@@ -34,8 +36,33 @@ app.use(userRoute);
 app.use(commentRoute);
 app.use(voteRoute);
 
-app.listen(PORT, () => {
+// create default users.
+app.listen(PORT, async () => {
   console.log(`[SERVER]: Running at http://localhost:${PORT}`);
+
+  async function createDefaultUser(username: string, email: string) {
+    const hasBeenCreated = await User.findOne({ username });
+    if (!hasBeenCreated) {
+
+      const response = await fetch("http://localhost:3001/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          email,
+          password: "1",
+          confirmPW: "1"
+        }),
+      })
+
+      if (response.ok) console.log(`successfully created ${username}.`);
+    }
+  }
+
+  createDefaultUser("ANTHIMON", "anthony@gmail.com")
+  createDefaultUser("Protea", "bro@gmail.com")
 });
 
 process.on('SIGINT', gracefulShutdown);
