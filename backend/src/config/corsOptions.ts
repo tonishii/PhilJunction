afde080db33d3d905df767ddc366c0432030ef60
RD-Tan <ross_tan@dlsu.edu.ph>
2025-03-19 11:24:25 +0800

    added port 4173 to cors

--- backend/src/config/corsOptions.ts
+++ backend/src/config/corsOptions.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsOptions = {
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000", "http://localhost:4173"]
 }
 
 export default cors(corsOptions);
