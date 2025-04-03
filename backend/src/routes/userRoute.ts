1e76334a3f7e3e3b6c392fdf22ed8942dfeee516
RD-Tan <ross_tan@dlsu.edu.ph>
2025-04-03 21:34:57 +0800

    removed a bunch of console.log

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -24,7 +24,7 @@ router.get('/randomuser', async (req: Request, res: Response) => {
 router.get('/user/:username', async (req: Request, res: Response): Promise<any> => {
     try {
         const user = await User.findOne({ username: req.params.username });
-        console.log(user, req.params.username)
+        // console.log(user, req.params.username)
         if (!user) {
             return res.status(404).json({ message: "User not found." });
         }
@@ -34,11 +34,10 @@ router.get('/user/:username', async (req: Request, res: Response): Promise<any> 
             imageUrl: `data:${user.icon.contentType};base64,${user.icon.data.toString('base64')}`
         };
         const userId = req.session.userId;
-        console.log(req.session);
+        // console.log(req.session);
 
         // checking if logged in.
         if (userId === undefined || userId === null) {
-            console.log("entered the thing");
             return res.status(200).json({ message: "User data successfully pulled", user: { ...user.toObject(), icon: icon }, logOut: true });
         }
         return res.status(200).json({ message: "User data successfully pulled", user: { ...user.toObject(), icon: icon } });
@@ -60,7 +59,7 @@ router.get('/user/:username/posts', async (req: Request, res: Response): Promise
         if (!posts || posts.length === 0) {
             return res.status(200).json({ message: "No posts yet. ", posts: [] });
         }
-        console.log(posts);
+        // console.log(posts);
         return res.status(200).json({ message: "Posts of user successfully pulled.", posts: posts });
     } catch (err) {
         res.status(500).json({ message: "Server error:", err });
@@ -90,7 +89,7 @@ router.get('/user/:username/comments', async (req: Request, res: Response): Prom
 router.post("/updateuser", IsLoggedIn, upload.single("icon"), async (req: Request, res: Response): Promise<any> => {
     try {
         const { oldusername, username, email, bio } = req.body;
-        if(oldusername != req.session.username) {
+        if (oldusername != req.session.username) {
             return res.status(403).json({ message: "Unauthorized action." });
         }
         if (!(email as string).match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
